282ec18d3124e6a007af1263f7757ae9229ae80a
mehrd <uuserton@gmail.com>
2024-03-15 14:18:34 +0330

    make shit work

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.style.scss'
+const Header = () => {
+  return (
+    <div className='forheader'>
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+
+    </div>
+  )
+}
+
+export default Header
